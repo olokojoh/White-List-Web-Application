51b6a7c6c5d7bcedb16dbaf16e020c7fc1839161
olokojoh <54641961+olokojoh@users.noreply.github.com>
2020-10-07 17:47:54 -0400

    config for pc

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/white_list"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Cjl52289011^@localhost:3306/white_list"
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://zguser:123456@10.20.21.83:3306/white_list"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WHOOSH_BASE = os.path.join(basedir, 'search.db')
